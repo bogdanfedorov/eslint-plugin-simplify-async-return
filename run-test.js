203595e2fb1f7c4d932512c68b835197bfafaa87
Bohdan Fedorv <bogdanfedorov@gmail.com>
2024-11-13 23:05:20 +0200

    remove test runer

--- run-test.js
+++
@@ -1,9 +0,0 @@
-import { execSync } from "child_process";
-
-try {
-  console.log("Running ESLint plugin tests...");
-  const output = execSync("npm test", { encoding: "utf8" });
-  console.log(output);
-} catch (error) {
-  console.error("Error running tests:", error.stdout);
-}
